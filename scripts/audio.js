2a6bb4f65da6249e63f98120226d616e3b5407c8
vasilkrug <vkruglovec@gmail.com>
2023-09-15 16:39:25 +0300

    add audio

---
+++ scripts/audio.js
@@ -0,0 +1,4 @@
+export const tetrisTheme = new Audio('../assets/audio/tetris-theme.mp3');
+export const gameOverAudio = new Audio('../assets/audio/game-over.mp3');
+export const moveAudio = new Audio('../assets/audio/move.mp3');
+export const removeRowAudio = new Audio('../assets/audio/remove-row.mp3');
